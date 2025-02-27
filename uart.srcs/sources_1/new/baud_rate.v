d9aa8a2bc0ef3f4116b67d1f2c648b29577d49ca
Ignacio <nacborgatello97@gmail.com>
2025-02-27 16:06:51 -0300

    second commit: added new features

--- uart.srcs/sources_1/new/baud_rate.v
+++ uart.srcs/sources_1/new/baud_rate.v
@@ -2,25 +2,23 @@
 
 module baud_rate
     #(    
-        parameter FREQ_CLK = 50000000,
-        parameter BAUD_RATE = 19200
+        parameter DVSR = 163,
+        parameter DVSR_BIT = 8
     )
     (
         input clk,
         input reset,
-        output reg o_tick
+        output wire o_tick
     );
     
-    localparam DIVISOR = FREQ_CLK / (BAUD_RATE * 16);
-    
-    reg [7:0] counter = 0;
+    reg [DVSR_BIT-1:0] counter = 0;
     
     always @(posedge clk or posedge reset) begin
         if (reset) begin
             counter <= 0;
             o_tick <= 0;
         end else begin
-            if (counter == DIVISOR) begin
+            if (counter == DVSR) begin
                 counter <= 0;
                 o_tick <= ~o_tick;
             end else begin
